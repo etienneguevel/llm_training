cbf27c9e60428771df519618fea265c7180bc95a
etienneguevel <etienneguev@gmail.com>
2026-05-13 18:02:51 +0200

    fix shape of input

--- llm_training/generate.py
+++ llm_training/generate.py
@@ -50,14 +50,14 @@ def main():
     else:
         txt = args.txt
 
-    print(f"text used is {txt}")
-    tokens = tokenizer(txt, return_tensors="pt")["input_ids"].squeeze(0)
+    tokens = tokenizer(txt, return_tensors="pt")["input_ids"]
 
-    print(f"shape of the tokens given to the model: {tokens.shape}")
 
     # Generate the new tokens
     output_tokens = model.generate(tokens, 20)
     output_txt = tokenizer.decode(output_tokens)
+
+    print(output_txt)
 
     return output_txt
 
